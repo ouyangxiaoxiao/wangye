cffc6c65d79520c6212b87b2bd11f80818d64cac
ouyangxiaoxiao <284473823@qq.com>
2021-05-10 15:23:18 +0800

    999

---
+++ 《零基础入门学习web开发》（HTML&CS33）/lesson0/love/js/audio.js
@@ -0,0 +1,7 @@
+document.addEventListener('touchstart', function () {
+    function audioAutoPlay() {
+        var audio = document.getElementById('audio');
+            audio.play();
+    }
+    audioAutoPlay();
+});
